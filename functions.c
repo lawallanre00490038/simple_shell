531da6648a75d4edb10ed34d1435593c5af6eec5
Lawal Olanrewaju Israel <61704062+lawallanre00490038@users.noreply.github.com>
2022-08-29 10:45:28 +0100

    Update functions.c

--- functions.c
+++ functions.c
@@ -9,7 +9,7 @@ int _strlen(const char *s)
 {
 int i = 0;
 while (s[i])
-	i++;
+i++;
 
 return (i);
 }
@@ -26,12 +26,12 @@ return (i);
 
 char *_strcpy(char *dest, const char *src)
 {
-	int i = -1;
+int i = -1;
 
-	do {
-		i++;
-		dest[i] = src[i];
-	} while (src[i] != '\0');
+do {
+i++;
+dest[i] = src[i];
+} while (src[i] != '\0');
 
-	return (dest);
+return (dest);
 }
